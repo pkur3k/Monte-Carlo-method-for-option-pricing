a588fb85a3f2c5769d64285d37821074e35672f3
pkur3k <piotr.j.kurek@gmail.com>
2023-10-05 20:16:13 +0100

    analyze data and compare to true values

--- MC_pricing.R
+++ MC_pricing.R
@@ -1,3 +1,36 @@
+MCPrice<-function(Np, Nt, T, r, sigma, s0, K) {
+  
+  dt = T/Nt
+  Z <-  matrix(rnorm(Np * Nt, mean=0, sd=1),nrow = Nt, ncol = Np) 
+  dB <- Z*sqrt(dt) 
+  B <- matrix(numeric(Np*(Nt+1)), nrow = (Nt+1), ncol = Np)
+  S <- matrix(numeric(Np*(Nt+1)), nrow = (Nt+1), ncol = Np)
+  
+  for(i in 1:Np){
+    B[,i] <- c(0, cumsum(dB[,i]))
+    S[,i] <- s0*exp((r - 0.5*sigma^2)*T + sigma*B[,i]) 
+  }
+  
+  ST <- S[nrow(S),]
+  
+  # call option price 
+  call_payoffs <- exp(-r*T)*pmax(ST-K,0)
+  call_price <- mean(call_payoffs)
+  
+  # put option price 
+  put_payoffs <- exp(-r*T)*pmax(K-ST,0)
+  put_price <- mean(put_payoffs)
+  
+  # standard deviation 
+  err_call <- sd(call_payoffs)/sqrt(Np)
+  err_put <- sd(put_payoffs)/sqrt(Np)
+  
+  output<-list(call_price=call_price, err_call=err_call, 
+               put_price=put_price, err_put=err_put)
+  
+  return(output)
+}
+
 sigma <- 0.3
 mu <- 0.05
 Nt <- 252
@@ -7,32 +40,26 @@ K <- 100
 r <- 0.1
 T <- 1
 
-MCPrice<-function(Np, T, r, sigma, s0, K) {
-  
-  z <- rnorm(Np, mean=0, sd=1)
-  S <- s0*exp((r - 0.5*sigma^2)*T + sigma*sqrt(T) * z)
-  
-  # call option price 
-  call_payoffs <- exp(-r*T)*pmax(S-K,0)
-  print(call_payoffs)
-  call_price <- mean(call_payoffs)
-  
-  # put option price and 
-  put_payoffs <- exp(-r*T)*pmax(K-S,0)
-  put_price <- mean(put_payoffs)
-  
-  # standard deviation 
-  err_call <- sd(call_payoffs)/sqrt(Np)
-  err_put <- sd(put_payoffs)/sqrt(Np)
-  
-  output<-list(call_price=call_price, err_call=err_call, 
-               put_price=put_price, err_call=err_call)
-  
-  return(output)
-  
-}
-
-results<-MCPrice(Np, T, r, sigma, s0, K)
+results<-MCPrice(Np, Nt, T, r, sigma, s0, K)
 results
-  
+
+# compare with true value
+library(RQuantLib)
+EuropeanOption("call", s0, K, 0, r, T, sigma)$value
+EuropeanOption("put", s0, K, 0, r, T, sigma)$value
+
+# for results = S
+matplot (results[,1:3], type="l", lty=1, col=c("green", "blue", "red"), 
+         main="Monte Carlo price paths", 
+         xlab="Time", ylab="Price")
+
+# for results = S
+hist(results[Nt+1,], main="Terminal values", 
+     xlab="Values", ylab="Frequency")
+
+# for results = call_payoffs
+hist(results, main="Terminal call values", 
+     xlab="Values", ylab="Frequency")
+
+
  
